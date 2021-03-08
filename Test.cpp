73e78d1366b0ba3f11107cf68caf4c6f92c9d69d
davidfeust <david.feust@gmail.com>
2021-03-08 12:25:02 +0200

    more test

--- Test.cpp
+++ Test.cpp
@@ -2,8 +2,6 @@
  * An example of how to write unit tests.
  * Use this as a basis to build a more complete Test.cpp file.
  * 
- * IMPORTANT: Please write more tests - the tests here are only for example and are not complete.
- *
  * AUTHORS: David Feust
  * 
  * Date: 2021-02
@@ -20,7 +18,10 @@ using namespace ariel;
 using namespace std;
 
 int arr_to_number(const int arr[], int n);
+
 int create_code();
+
+int create_code(int index, int val);
 
 /*
  HHHHH
@@ -112,29 +113,93 @@ TEST_CASE ("More test") {
                                               " (\" \") \n"));
 }
 
+TEST_CASE ("Hats") {
+    const string hat[] = {"       \n _===_ \n", "  ___  \n ..... \n", "   _   \n  /_\\  \n", "  ___  \n (_*_) \n"};
+    for (int i = 1; i <= 4; ++i) {
+        int num = create_code(0, i);
+        CHECK(snowman(num).substr(0, 16) == hat[i - 1]);
+    }
+}
 
-//TEST_CASE("Bad snowman code") {
-//            CHECK_THROWS(snowman(555));
-//            CHECK_THROWS(snowman(222222222)); // 9 digits
-//            CHECK_THROWS(snowman(2222222)); // 7 digits
-//            CHECK_THROWS(snowman(123321)); // short number
-//            CHECK_THROWS(snowman(11118111)); // invalid digit (8)
-//            CHECK_THROWS(snowman(-31433313)); // negative number
-//            CHECK_THROWS(snowman(0)); // zero
-//}
+TEST_CASE ("Nose") {
+    const string nose[] = {",", ".", "_", " "};
+    for (int i = 1; i <= 4; ++i) {
+        int num = create_code(1, i);
+        CHECK(snowman(num).substr(19, 1) == nose[i - 1]);
+    }
+}
 
-//
-//TEST_CASE ("More then 8 digits") {
-//    for (int i = 0; i < 20; ++i) {
-//        int num = 10000000;
-//        int random = (int) rand();
-//        num += random;
-//        CHECK_THROWS(snowman(num));
-//    }
-//}
+TEST_CASE ("Eyes") {
+    const string eye[] = {".", "o", "O", "-"};
+    for (int i = 1; i <= 4; ++i) {
+        int num = create_code(2, i);
+        CHECK(snowman(num).substr(18, 1) == eye[i - 1]);
+        num = create_code(3, i);
+        CHECK(snowman(num).substr(20, 1) == eye[i - 1]);
+    }
+}
+
+TEST_CASE ("Left Arm") {
+    const string left_arm_up[] = {" ", "\\", " ", " "};
+    const string left_arm_down[] = {"<", " ", "/", " "};
+    for (int i = 1; i <= 4; ++i) {
+        int num = create_code(4, i);
+        const string &snowmen_res = snowman(num);
+        CHECK(snowmen_res.substr(16, 1) == left_arm_up[i - 1]);
+        CHECK(snowmen_res.substr(24, 1) == left_arm_down[i - 1]);
+    }
+}
+
+TEST_CASE ("Right Arm") {
+    const string right_arm_up[] = {" ", "/", " ", " "};
+    const string right_arm_down[] = {">", " ", "\\", " "};
+    for (int i = 1; i <= 4; ++i) {
+        int num = create_code(5, i);
+        const string &snowmen_res = snowman(num);
+        CHECK(snowmen_res.substr(22, 1) == right_arm_up[i - 1]);
+        CHECK(snowmen_res.substr(30, 1) == right_arm_down[i - 1]);
+    }
+}
+
+TEST_CASE ("Torso") {
+    const string torso[] = {" : ", "] [", "> <", "   "};
+    for (int i = 1; i <= 4; ++i) {
+        int num = create_code(6, i);
+        CHECK(snowman(num).substr(26, 3) == torso[i - 1]);
+    }
+}
+
+TEST_CASE ("Base") {
+    const string base[] = {" : ", "\" \"", "___", "   "};
+    for (int i = 1; i <= 4; ++i) {
+        int num = create_code(7, i);
+        CHECK(snowman(num).substr(34, 3) == base[i - 1]);
+    }
+}
 
 
-TEST_CASE("Invalid digit") {
+TEST_CASE ("Bad snowman code") {
+            CHECK_THROWS(snowman(555));
+            CHECK_THROWS(snowman(222222222)); // 9 digits
+            CHECK_THROWS(snowman(2222222)); // 7 digits
+            CHECK_THROWS(snowman(123321)); // short number
+            CHECK_THROWS(snowman(11118111)); // invalid digit (8)
+            CHECK_THROWS(snowman(-31433313)); // negative number
+            CHECK_THROWS(snowman(0)); // zero
+}
+
+
+TEST_CASE ("More then 8 digits") {
+    for (int i = 0; i < 20; ++i) {
+        int num = 10000000;
+        int random = (int) rand();
+        num += random;
+        CHECK_THROWS(snowman(num));
+    }
+}
+
+
+TEST_CASE ("Invalid digit") {
     for (int i = 5; i <= 10; ++i) {
         for (int j = 0; j < 8; ++j) {
             int num[] = {1, 1, 1, 1, 1, 1, 1, 1};
@@ -144,7 +209,7 @@ TEST_CASE("Invalid digit") {
                 num[j] = i;
             }
             int n = arr_to_number(num, 8);
-            CHECK_THROWS(snowman(n));
+                    CHECK_THROWS(snowman(n));
         }
     }
 }
@@ -192,5 +257,15 @@ int create_code() {
         int r = (rand() % 4) + 1;
         nums[i] = r;
     }
+    return arr_to_number(nums, 8);
+}
+
+int create_code(int index, int val) {
+    int nums[8];
+    for (int i = 0; i < 8; ++i) {
+        int r = (rand() % 4) + 1;
+        nums[i] = r;
+    }
+    nums[7 - index] = val;
     return arr_to_number(nums, 8);
 }
